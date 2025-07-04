9cf0d01e5e0c2bd69bd21dbc136eca253ad0a388
[dyad] <21abibp@gmail.com>
2025-07-04 19:46:22 +0530

    [dyad] Added animated background - wrote 2 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,10 +75,16 @@ export default {
             height: "0",
           },
         },
+        "gradient-animation": {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "gradient": "gradient-animation 20s ease infinite",
       },
     },
   },
